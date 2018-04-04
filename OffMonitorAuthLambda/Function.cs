29e4d524eb00a942569b9a441a9912692bd5e173
vaidaabelkyte <vaidilutia@gmail.com>
2018-04-04 20:42:17 +0100

    Empty lambda function is pushed to AWS service correctly

---
+++ OffMonitorAuthLambda/Function.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace OffMonitorAuthLambda
+{
+    public class Function
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public object FunctionHandler(object input, ILambdaContext context)
+        {
+            return input;
+        }
+    }
+}
